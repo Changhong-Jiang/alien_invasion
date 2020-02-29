9685f4a34493fd0e0547e274d94be5fb57336eb1
Changhong-Jiang <578847606@qq.com>
2020-02-29 18:37:06 +0800

    add alien_invasion

---
+++ alien_invasion.py
@@ -0,0 +1,52 @@
+import sys
+import pygame
+from pygame.sprite import Group
+
+from settings import Settings
+from ship import Ship
+from alien import Alien
+from game_stats import GameStats
+from scoreboard import Scoreboard
+from button import Button
+import game_functions as gf
+
+def run_game():
+    # 初始化pygame，设置，并创建一个屏幕对象
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invation by Changhong")
+    # 创建play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
+    # 创建一个用于存储游戏统计信息的实例，创建记分牌
+    stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
+    # 设置背景颜色
+    bg_color = (230, 230, 230)
+
+    # 创建飞船，储存子弹的编组，储存外星人的编组
+    ship = Ship(ai_settings, screen)
+    bullets = Group()
+    aliens = Group()
+
+    # 创建外星人群
+    # alien = Alien(ai_settings, screen)
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # 开始游戏主循环
+    while True:
+        # 监视键盘和鼠标事件
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
+        if stats.game_active:
+            # 更新飞船位置
+            ship.update()
+            # 更新子弹位置并消除到顶子弹
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            # 更新外星人群位置
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        # 更新屏幕
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
+
+run_game()
